31709a998b92012ba88e303e575ccc85798f1742
Janto Dreijer <jantod@gmail.com>
2021-11-02 18:22:10 +0200

    initial add

---
+++ eth_install.sh
@@ -0,0 +1,5 @@
+# https://geth.ethereum.org/docs/install-and-build/installing-geth#install-on-ubuntu-via-ppas
+
+sudo add-apt-repository -y ppa:ethereum/ethereum
+sudo apt-get update
+sudo apt-get install ethereum
